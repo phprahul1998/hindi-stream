307ff80f6a2cf80ebbbd0a5b10bba87fb6d533ee
Rahul Kumar <rahul.kumar@softage.net>
2023-08-23 13:02:55 +0530

    upload

---
+++ app/api/chat/route.ts
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: ' 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',
+});
+const openai = new OpenAIApi(config);
+const rr= 'What is india?'
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { vibe, bio } = await req.json();
+
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [
+      {
+        role: 'user',
+        content: `Plese response in hindi ${rr}`,
+      },
+    ],
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
